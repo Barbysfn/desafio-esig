5533bf6a2620d6f081e968fb5a5e3ccb4d87934b
Barbys <barbara_fn@live.com>
2025-03-03 18:42:33 -0300

    aplicação de testes

---
+++ gerenciador-tarefas-core/src/test/java/com/gerenciador/esig/TarefaTest.java
@@ -0,0 +1,12 @@
+package com.gerenciador.esig;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TarefaTest {
+
+    @Test
+    void testExample() {
+        assertTrue(true);
+    }
+}
---
+++ gerenciador-tarefas-web/src/test/java/com/gerenciador/esig/TarefaBeanTest.java
@@ -0,0 +1,12 @@
+package com.gerenciador.esig;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TarefaBeanTest {
+
+    @Test
+    void testExample() {
+        assertTrue(true);
+    }
+}
